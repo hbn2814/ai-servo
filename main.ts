6c30fce2e491460275425e38a543763448f76b21
hbn2814 <calmman1206@gmail.com>
2026-08-08 11:51:05 +0900

    MakeCode 프로젝트용 기초 파일

---
+++ main.ts
@@ -0,0 +1,22 @@
+bluetooth.onUartDataReceived(serial.delimiters(Delimiters.NewLine), function () {
+    data = bluetooth.uartReadUntil(serial.delimiters(Delimiters.NewLine))
+    if (data == "ID1") {
+        servos.P1.setAngle(0)
+    }
+    if (data == "ID2") {
+        servos.P1.setAngle(90)
+    }
+    if (data == "ID3") {
+        servos.P1.setAngle(180)
+    }
+})
+bluetooth.onBluetoothConnected(function () {
+    basic.showIcon(IconNames.Heart)
+    music.play(music.builtinPlayableSoundEffect(soundExpression.hello), music.PlaybackMode.UntilDone)
+})
+bluetooth.onBluetoothDisconnected(function () {
+    basic.showIcon(IconNames.Sad)
+    music.play(music.builtinPlayableSoundEffect(soundExpression.sad), music.PlaybackMode.UntilDone)
+})
+let data = ""
+basic.showString(control.deviceName())
